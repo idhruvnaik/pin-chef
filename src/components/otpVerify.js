6a1d35b43853c229a16096346016c551f2c98616
Dhaval Khemani <Dhaval.Khemani@sophos.com>
2021-03-10 11:25:03 +0530

    Added apiOperations for login and otp verify.

--- src/components/otpVerify.js
+++ src/components/otpVerify.js
@@ -1,41 +1,44 @@
 import React, { useState } from 'react';
 import { useLocation } from "react-router-dom";
 import { useSpring, animated } from "react-spring";
-import { getSettingsAuth } from '../services/apiOperations';
-import { connect } from "react-redux";
-import { addToken, donotAddToken } from "../services/actions";
-import Popup from 'reactjs-popup';
+import { verifyOtp } from '../services/apiOperations';
 import './otpVerify.css'
 import OtpInput from 'react-otp-input';
+import IncorrectPin from '../assets/png_icons/incorrect pin icon.png'
 import OTP from './otpFields'
-// import 'reactjs-popup/dist/index.css';
 
 export default function VerifyOTP() {
     const location = useLocation();
-    var external_otp = '';
-    console.log(location.email);
-    function handleChange(otp){
-        console.log(otp);
-        console.log(typeof(external_otp));
-        external_otp = otp;
-        console.log(typeof(external_otp));
-        console.log(external_otp);
-    }
-    function saveotp(otp){
-        console.log(otp);
-        console.log("otp aayu");
+
+    function saveotp(){
+        var username = location.email;
+        var otp = "123456";
+        verifyOtp(username, otp).then(function(result){
+            if (result){
+                console.log(result);
+                if(result.status == false){
+                    console.log("in status false if");
+                    var submit_otp = document.querySelector('.verify_otp button');
+                    // submit_otp.innerHTML = "!";
+                    // submit_otp.style.backgroundColor = "#D62929";
+                    submit_otp.style.backgroundImage = IncorrectPin;
+                    // submit_otp.style.width = "1.5em";   
+                }
+            }
+        })
+        
     }
     return (
         <div className="outer-layout" style={{backgroundColor: "#555", backgroundImage: "none" }}>
             <div className="otp-container">
                 <div className="login-register">
                     <div className="heading">VERIFY</div>
-                    <div className="user_mail">marry@yahoo.com</div>
+                    <div className="user_mail">{location.email}</div>
                     <div className="tooltip">Enter 6 digits OTP</div>
                     <div className="actual_otp">
                         <OTP/>
                         <div className="verify_otp">
-                            <button type="button">OK</button>
+                            <button type="button" onClick={saveotp} style={{backgroundImage: "url(IncorrectPin)", backgroundSize: "cover", backgroundPosition: "center center"}}>OK</button>
                         </div>
                     </div>
                     <div className="resend_email">Resend Email</div>
