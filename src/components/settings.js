5e40ead65f5fdc30136a85e0b84fa9a3768ca737
Dhaval Khemani <Dhaval.Khemani@sophos.com>
2021-03-20 11:01:52 +0530

    Added setting componenets.

--- src/components/settings.js
+++ src/components/settings.js
@@ -6,11 +6,20 @@ import PostMenu from '../assets/png_icons/Post menu icon@2x.png';
 import CommentIcon from '../assets/png_icons/Comment icon@2x.png';
 import EmptyHeart from '../assets/png_icons/Empty heart@2x.png';
 import PostShare from '../assets/png_icons/Post Share count@2x.png';
+import SmallRightArrow from '../assets/png_icons/Right green arrow.png'
 import RightArrow from '../assets/png_icons/Right green arrow@2x.png';
+import LeftBack from '../assets/png_icons/Green back arrow.png'
 import BellIcon from '../assets/png_icons/Bell icon@2x.png'
 import Help from '../assets/png_icons/Help@2x.png'
 import Share from '../assets/png_icons/share settings@2x.png'
 import Star from '../assets/png_icons/Yellow star@2x.png'
+import FAQ from '../assets/png_icons/Faq icon on menu.png'
+import Terms from '../assets/png_icons/terms icon on menu.png'
+import ContactUs from '../assets/png_icons/contact-us icon on menu.png'
+import LargeFAQ from '../assets/png_icons/Faq icon on menu@2x.png'
+import LargeTerms from '../assets/png_icons/terms icon on menu@2x.png'
+import LargeContactUs from '../assets/png_icons/contact-us icon on menu@2x.png'
+import TermsLogo from '../assets/png_icons/terms and privacy bullet icon@2x.png'
 import Home from './home'
 
 import Switch from "react-switch";
@@ -139,9 +148,17 @@ export default class settings extends Component {
         this.setState({ checked });
     }
 
-    open_help() {
-        $('.profile').css('display', 'none');
-        $('.help').css('display', 'block');
+    open_menu(menu_class) {
+        var menu_siblings = $('.'+menu_class).siblings();
+        console.log(menu_siblings);
+        menu_siblings.each(function(){
+            $( this ).css('display', 'none');
+        })
+        // for(i=0; i < menu_siblings.length; i++){
+        //     menu_siblings[i].css('display', 'none');
+        // }
+        // $('.profile').css('display', 'none');
+        $('.'+menu_class).css('display', 'block');
     }
 
     render() {
@@ -180,7 +197,7 @@ export default class settings extends Component {
                             <Switch onChange={this.handleChange} checked={this.state.checked} uncheckedIcon={false} checkedIcon={false} />
                         </div>
                     </div>
-                    <div className="setting_activity" onClick={this.open_help}>
+                    <div className="setting_activity" onClick={() => this.open_menu('help')}>
                         <div className="menu_details">
                             <div className="menu_icon">
                                 <img src={Help}></img>
@@ -215,20 +232,171 @@ export default class settings extends Component {
                     </div>
                 </div>
                 <div className="help">
-                    <ul className="switch-content">
-                        <li onClick={this.active} className="nav-active" id="food">MY PROFILE</li>
-                    </ul>
-                    {/* <h3>tamili habibi mastibini hear</h3> */}
-                    {/* <PushMenu
-                        backIcon={<RightArrow />}
-                        openOnMount
-                        expanderComponent={RightArrow}
-                        linkComponent={Home}
-                        nodes={this.data.menu}
-                        propMap={{ url: 'link' }}
-                    >
-                        <Home />
-                    </PushMenu> */}
+                    <div className="switch-content">
+                        <div>
+                            <img src={LeftBack} onClick={() => this.open_menu('profile')}></img>
+                        </div>
+                        <div>
+                            <h2>HELP</h2>
+                        </div>
+                    </div>
+                    <div className="help_activity" onClick={() => this.open_menu('faq')}>
+                        <div className="menu_details">
+                            <div className="menu_icon">
+                                <img src={LargeFAQ}></img>
+                            </div>
+                            <div className="menu_name">
+                                <h3>FAQ</h3>
+                            </div>
+                        </div>
+                        <div className="other_things">
+                            <img src={SmallRightArrow}></img>
+                        </div>
+                    </div>
+                    <div className="help_activity" onClick={() => this.open_menu('terms_privacy_policy')}>
+                        <div className="menu_details">
+                            <div className="menu_icon">
+                                <img src={LargeTerms}></img>
+                            </div>
+                            <div className="menu_name">
+                                <h3>terms &nbsp; privacy policy</h3>
+                            </div>
+                        </div>
+                        <div className="other_things">
+                            <img src={SmallRightArrow}></img>
+                        </div>
+                    </div>
+                    <div className="help_activity" onClick={() => this.open_menu('contact_us')}>
+                        <div className="menu_details">
+                            <div className="menu_icon">
+                                <img src={LargeContactUs}></img>
+                            </div>
+                            <div className="menu_name">
+                                <h3>contact us</h3>
+                            </div>
+                        </div>
+                        <div className="other_things">
+                            <img src={SmallRightArrow}></img>
+                        </div>
+                    </div>
+                </div>
+                <div className="faq">
+                    <div className="switch-content">
+                        <div>
+                            <img src={LeftBack} onClick={() => this.open_menu('help')}></img>
+                        </div>
+                        <div>
+                            <img src={FAQ} style={{marginRight:"5px"}}></img>
+                            <h3>FAQ</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className="terms_privacy_policy">
+                    <div className="switch-content">
+                        <div>
+                            <img src={LeftBack} onClick={() => this.open_menu('help')}></img>
+                        </div>
+                        <div>
+                            <img src={Terms} style={{marginRight:"5px"}}></img>
+                            <h3>TERMS &amp; PRIVACY POLICY</h3>
+                        </div>
+                    </div>
+                    <div className="policies">
+                        <div className="policy">
+                            <div>
+                                <img src={TermsLogo}></img>
+                            </div>
+                            <div>
+                                <h3>Age Restriction Rules</h3>
+                                <p>You have to create a new post for a screening. After this you can add the file you want to go live with or just use your camera to do it.</p>
+                                <p>You have to create a new post for a screening. After this you can add the file you want to go live with or just use your camera to do it.</p>
+                                <p>You have to create a new post for a screening. After this you can add the file you want to go live with or just use your camera to do it.</p>
+                            </div>
+                        </div>
+                        <div className="policy">
+                            <div>
+                                <img src={TermsLogo}></img>
+                            </div>
+                            <div>
+                                <h3>California Law</h3>
+                                <p>You have to create a new post for a screening. After this you can add the file you want to go live with or just use your camera to do it.</p>
+                            </div>
+                        </div>
+                        <div className="policy">
+                            <div>
+                                <img src={TermsLogo}></img>
+                            </div>
+                            <div>
+                                <h3>Age Restriction Rules</h3>
+                                <p>You have to create a new post for a screening. After this you can add the file you want to go live with or just use your camera to do it.</p>
+                                <p>You have to create a new post for a screening. After this you can add the file you want to go live with or just use your camera to do it.</p>
+                                <p>You have to create a new post for a screening. After this you can add the file you want to go live with or just use your camera to do it.</p>
+                            </div>
+                        </div>
+                        <div className="policy">
+                            <div>
+                                <img src={TermsLogo}></img>
+                            </div>
+                            <div>
+                                <h3>Age Restriction Rules</h3>
+                                <p>You have to create a new post for a screening. After this you can add the file you want to go live with or just use your camera to do it.</p>
+                                <p>You have to create a new post for a screening. After this you can add the file you want to go live with or just use your camera to do it.</p>
+                                <p>You have to create a new post for a screening. After this you can add the file you want to go live with or just use your camera to do it.</p>
+                            </div>
+                        </div>
+                        <div className="policy">
+                            <div>
+                                <img src={TermsLogo}></img>
+                            </div>
+                            <div>
+                                <h3>Age Restriction Rules</h3>
+                                <p>You have to create a new post for a screening. After this you can add the file you want to go live with or just use your camera to do it.</p>
+                                <p>You have to create a new post for a screening. After this you can add the file you want to go live with or just use your camera to do it.</p>
+                                <p>You have to create a new post for a screening. After this you can add the file you want to go live with or just use your camera to do it.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="contact_us">
+                    <div className="switch-content">
+                        <div>
+                            <img src={LeftBack} onClick={() => this.open_menu('help')}></img>
+                        </div>
+                        <div>
+                            <img src={ContactUs} style={{marginRight:"5px"}}></img>
+                            <h3>CONTACT US</h3>
+                        </div>
+                    </div>
+                    <div style={{height:"100%"}}>
+                        <div className="instruction">
+                            <div>
+                                <img src={TermsLogo}></img>
+                            </div>
+                            <div>
+                                <h3>Please fill the form below and submit suggestion, feedback, or any advertisement related questions</h3>
+                            </div>
+                        </div>
+                        <div className="feedback">
+                            <form action='' id="feedbackform" onSubmit={this.login_user}>
+                                <div>
+                                    <h3>Name</h3>
+                                    <input type="text" id="username" placeholder="Enter name"></input>
+                                </div>
+                                <div>
+                                    <h3>Email<span style={{color:"red"}}> *</span></h3>
+                                    <input type="text" id="email" placeholder="Enter email" required></input>
+                                </div>
+                                <div>
+                                    <h3>Subject</h3>
+                                    <input type="text" id="subject" placeholder="Enter subject"></input>
+                                </div>
+                                <div>
+                                    <h3>Message<span style={{color:"red"}}> *</span></h3>
+                                    <input type="text" id="subject" placeholder="Enter message" required></input>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         );  
