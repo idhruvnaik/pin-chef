4f1ade55cf1b3f33dfe4d93268309990ea4cbebf
Dhaval Khemani <Dhaval.Khemani@sophos.com>
2021-04-21 22:29:02 +0530

    Changed forgot password.

--- src/components/forgotPassword.js
+++ src/components/forgotPassword.js
@@ -6,7 +6,9 @@ import './forgotPassword.scss'
 import OtpInput from 'react-otp-input';
 import IncorrectPin from '../assets/png_icons/incorrect pin icon.png'
 import Email from '../assets/svg/Email icon.svg';
+import EmailIcon from '../assets/svg/resend-otp-email.svg';
 import FPBack from '../assets/svg/fp-back-icon.svg';
+import Popup from 'reactjs-popup';
 import $ from 'jquery';
 
 const mapStateToProps = state => ({
@@ -23,6 +25,11 @@ class ForgotPassword extends React.Component {
         super(props);
         this.get_password = this.get_password.bind(this);
         this.back_to_login = this.back_to_login.bind(this);
+        this.go_to_otp_verification = this.go_to_otp_verification.bind(this);
+        this.state = {
+            email_popup_flag: false,
+            current_email: null
+        };
     }
 
     async get_password(event) {
@@ -38,13 +45,12 @@ class ForgotPassword extends React.Component {
                     $('#errorMessage')[0].innerHTML = result.message;
                 }
             } else {
-                this.props.history.push(
-                    {            
-                        pathname: '/Verifyotp',
-                        email: result.email,
-                        redirect: "/User/ResetPassword"
-                    }
-                );
+                this.setState({
+                    current_email: result.email
+                });
+                this.setState({
+                    email_popup_flag: true
+                });
             }
         } else {
             $('#errorMessage')[0].innerHTML = "Enter Email or USER ID";
@@ -52,8 +58,19 @@ class ForgotPassword extends React.Component {
         console.log(username, "from forgot password");
     }
 
-    back_to_login(){
+    back_to_login() {
         this.props.history.goBack();
+    }
+
+    go_to_otp_verification() {
+        this.props.history.push(
+            {            
+                pathname: '/Verifyotp',
+                email: this.state.current_email,
+                redirect: "/User/ResetPassword"
+            }
+        );
+
     }
 
     render() {
@@ -88,6 +105,27 @@ class ForgotPassword extends React.Component {
                                 <input type="submit" value="Continue" className="submit"></input>
                             </div>
                         </form>
+                        <Popup
+                            open={this.state.email_popup_flag}
+                            position="center center"
+                            closeOnDocumentClick
+                            modal
+                            nested
+                        >
+                            {close => (
+                                <div className="resend-otp-popup">
+                                    <div className="icon">
+                                        <img src={EmailIcon}></img>
+                                    </div>
+                                    <div className="pop-up-instructions">
+                                        OTP sent once more to your registered email. Please check your email and try again.
+                                </div>
+                                    <div className="actions">
+                                        <button type="button" onClick={this.go_to_otp_verification}>OK</button>
+                                    </div>
+                                </div>
+                            )}
+                        </Popup>
                     </div>
                 </div>
             </div>
